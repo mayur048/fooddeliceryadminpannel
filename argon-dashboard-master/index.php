6f458a42ac59efc13088b98ef17f5e5db5da0407
anilpandey071999 <31802461+anilpandey071999@users.noreply.github.com>
2021-02-10 23:47:23 +0530

    login with Session

--- argon-dashboard-master/index.php
+++ argon-dashboard-master/index.php
@@ -1,3 +1,33 @@
+<?php 
+  include 'config.php';
+  if (isset($_POST['submit'])) {
+    $adminEmail = $_POST['Email'];
+    $adminPassword = $_POST['Password'];
+    $dbQuery = mysqli_query($db,"SELECT * FROM `login` WHERE `email` = '$adminEmail'");
+    $dbResult = "";
+    if (mysqli_num_rows($dbQuery) == 0) {
+      $messages = "No account Exists with this Email!! ";
+      echo "<script type='text/javascript'>alert('$messages');window.location.replace('index.php');</script>";
+    } else {
+      $dbResult = mysqli_fetch_assoc($dbQuery);
+      if ($adminPassword != $dbResult['password']) {
+        $messages = "Wrong Email or Password!! Try login!! ";
+        echo "<script type='text/javascript'>alert('$messages');window.location.replace('index.php');</script>";
+      } else {
+        if (dbResult['isActive'] == 1) {
+          session_start();
+          $_SESSION['Email'] = $adminEmail;
+          $_SESSION['Role'] = $dbResult['roleId'];
+          $messages = "Welcome!! ";
+          echo "<script type='text/javascript'>alert('$messages');window.location.replace('dashboard.php');</script>";
+        } else {
+          $messages = "You access has been Revoked!! ";
+          echo "<script type='text/javascript'>alert('$messages');window.location.replace('dashboard.php');</script>";
+        }
+      }
+    }
+  }
+?>
 <!DOCTYPE html>
 <html>
 
@@ -38,85 +68,6 @@
 
 <body class="bg-default">
   <!-- Navbar -->
-  <!-- <nav id="navbar-main" class="navbar navbar-horizontal navbar-transparent navbar-main navbar-expand-lg navbar-light">
-    <div class="container">
-      <a class="navbar-brand" href="dashboard.html">
-        <img src="../assets/img/brand/white.png">
-      </a>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar-collapse" aria-controls="navbar-collapse" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="navbar-collapse navbar-custom-collapse collapse" id="navbar-collapse">
-        <div class="navbar-collapse-header">
-          <div class="row">
-            <div class="col-6 collapse-brand">
-              <a href="dashboard.html">
-                <img src="../assets/img/brand/blue.png">
-              </a>
-            </div>
-            <div class="col-6 collapse-close">
-              <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navbar-collapse" aria-controls="navbar-collapse" aria-expanded="false" aria-label="Toggle navigation">
-                <span></span>
-                <span></span>
-              </button>
-            </div>
-          </div>
-        </div>
-        <ul class="navbar-nav mr-auto">
-          <li class="nav-item">
-            <a href="dashboard.html" class="nav-link">
-              <span class="nav-link-inner--text">Dashboard</span>
-            </a>
-          </li>
-          <li class="nav-item">
-            <a href="login.html" class="nav-link">
-              <span class="nav-link-inner--text">Login</span>
-            </a>
-          </li>
-          <li class="nav-item">
-            <a href="register.html" class="nav-link">
-              <span class="nav-link-inner--text">Register</span>
-            </a>
-          </li>
-        </ul>
-        <hr class="d-lg-none" />
-        <ul class="navbar-nav align-items-lg-center ml-lg-auto">
-          <li class="nav-item">
-            <a class="nav-link nav-link-icon" href="https://www.facebook.com/creativetim" target="_blank" data-toggle="tooltip" data-original-title="Like us on Facebook">
-              <i class="fab fa-facebook-square"></i>
-              <span class="nav-link-inner--text d-lg-none">Facebook</span>
-            </a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link nav-link-icon" href="https://www.instagram.com/creativetimofficial" target="_blank" data-toggle="tooltip" data-original-title="Follow us on Instagram">
-              <i class="fab fa-instagram"></i>
-              <span class="nav-link-inner--text d-lg-none">Instagram</span>
-            </a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link nav-link-icon" href="https://twitter.com/creativetim" target="_blank" data-toggle="tooltip" data-original-title="Follow us on Twitter">
-              <i class="fab fa-twitter-square"></i>
-              <span class="nav-link-inner--text d-lg-none">Twitter</span>
-            </a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link nav-link-icon" href="https://github.com/creativetimofficial" target="_blank" data-toggle="tooltip" data-original-title="Star us on Github">
-              <i class="fab fa-github"></i>
-              <span class="nav-link-inner--text d-lg-none">Github</span>
-            </a>
-          </li>
-          <li class="nav-item d-none d-lg-block ml-lg-4">
-            <a href="https://www.creative-tim.com/product/argon-dashboard-pro?ref=ad_upgrade_pro" target="_blank" class="btn btn-neutral btn-icon">
-              <span class="btn-inner--icon">
-                <i class="fas fa-shopping-cart mr-2"></i>
-              </span>
-              <span class="nav-link-inner--text">Upgrade to PRO</span>
-            </a>
-          </li>
-        </ul>
-      </div>
-    </div>
-  </nav> -->
   <!-- Main content -->
   <div class="main-content">
     <!-- Header -->
@@ -209,22 +160,6 @@
             &copy; 2021 <a class="font-weight-bold ml-1" target="_blank">Creative Team</a>
           </div>
         </div>
-        <!-- <div class="col-xl-6">
-          <ul class="nav nav-footer justify-content-center justify-content-xl-end">
-            <li class="nav-item">
-              <a href="https://www.creative-tim.com" class="nav-link" target="_blank">Creative Tim</a>
-            </li>
-            <li class="nav-item">
-              <a href="https://www.creative-tim.com/presentation" class="nav-link" target="_blank">About Us</a>
-            </li>
-            <li class="nav-item">
-              <a href="http://blog.creative-tim.com" class="nav-link" target="_blank">Blog</a>
-            </li>
-            <li class="nav-item">
-              <a href="https://github.com/creativetimofficial/argon-dashboard/blob/master/LICENSE.md" class="nav-link" target="_blank">MIT License</a>
-            </li>
-          </ul>
-        </div> -->
       </div>
     </div>
   </footer>
