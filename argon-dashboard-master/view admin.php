78b12868739c0452640e0cb2aa60895098407007
Mayur Shah <mayurshah048@gmail.com>
2021-02-17 00:58:50 +0530

    Update view admin.php

--- argon-dashboard-master/view admin.php
+++ argon-dashboard-master/view admin.php
@@ -30,29 +30,30 @@
 
     <!-- Header -->
     <!-- Header -->
-    <div class="header bg-primary pb-6">
-      <?php include("header.php");?>
-      <div class="container-fluid">
-        <div class="header-body">
-          <div class="row align-items-center py-4">
-            <div class="col-lg-6 col-7">
-              <h6 class="h2 text-white d-inline-block mb-0">View Admins</h6>
-              <!-- <nav aria-label="breadcrumb" class="d-none d-md-inline-block ml-md-4">
-                <ol class="breadcrumb breadcrumb-links breadcrumb-dark">
-                  <li class="breadcrumb-item"><a href="#"><i class="fas fa-home"></i></a></li>
-                  <li class="breadcrumb-item"><a href="#">Tables</a></li>
-                  <li class="breadcrumb-item active" aria-current="page">Tables</li>
-                </ol>
-              </nav> -->
-            </div>
-            <div class="col-lg-6 col-5 text-right">
-              <a href="#" class="btn btn-sm btn-neutral">Add Admin</a>
-             <!--  <a href="#" class="btn btn-sm btn-neutral">Filters</a> -->
+    <div class="main-content" id="panel">
+    <?php include("header.php");?>
+      <div class="header bg-primary pb-6">
+        <div class="container-fluid">
+          <div class="header-body">
+            <div class="row align-items-center py-4">
+              <div class="col-lg-6 col-7">
+                <h6 class="h2 text-white d-inline-block mb-0">View Admins</h6>
+                <!-- <nav aria-label="breadcrumb" class="d-none d-md-inline-block ml-md-4">
+                  <ol class="breadcrumb breadcrumb-links breadcrumb-dark">
+                    <li class="breadcrumb-item"><a href="#"><i class="fas fa-home"></i></a></li>
+                    <li class="breadcrumb-item"><a href="#">Tables</a></li>
+                    <li class="breadcrumb-item active" aria-current="page">Tables</li>
+                  </ol>
+                </nav> -->
+              </div>
+              <div class="col-lg-6 col-5 text-right">
+                <a href="#" class="btn btn-sm btn-neutral">Add Admin</a>
+               <!--  <a href="#" class="btn btn-sm btn-neutral">Filters</a> -->
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
     <!-- Page content -->
     <div class="container-fluid mt--6">
       <div class="row">
@@ -234,9 +235,11 @@
                 </tfoot>
               </table>
             </div>
+          </div>
+        </div>
+      </div>
       <!-- Footer -->
       <?php include("footer.php"); ?>
-
     </div>
   </div>
 
