66aaa717cfaf2da925f92b50a50d9f9b3bbd20b6
anilpandey071999 <31802461+anilpandey071999@users.noreply.github.com>
2021-02-16 23:51:27 +0530

    remove all commented code

--- argon-dashboard-master/add category.php
+++ argon-dashboard-master/add category.php
@@ -31,7 +31,6 @@
   <nav class="navbar navbar-top navbar-expand navbar-dark bg-primary border-bottom">
       <div class="container-fluid">
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        
           <!-- Navbar links -->
           <ul class="navbar-nav align-items-center  ml-md-auto ">
             <li class="nav-item d-xl-none">
@@ -83,19 +82,7 @@
         <div class="header-body">
           <div class="row align-items-center py-4">
             <div class="col-lg-6 col-7">
-              <!-- <h6 class="h2 text-white d-inline-block mb-0">Admins</h6>
-              <nav aria-label="breadcrumb" class="d-none d-md-inline-block ml-md-4">
-                <ol class="breadcrumb breadcrumb-links breadcrumb-dark">
-                  <li class="breadcrumb-item"><a href="#"><i class="fas fa-home"></i></a></li>
-                  <li class="breadcrumb-item"><a href="#">Tables</a></li>
-                  <li class="breadcrumb-item active" aria-current="page">Tables</li>
-                </ol>
-              </nav> -->
             </div>
-            <!-- <div class="col-lg-6 col-5 text-right">
-              <a href="#" class="btn btn-sm btn-neutral">Add Admin</a>
-              <a href="#" class="btn btn-sm btn-neutral">Filters</a>
-            </div> -->
           </div>
         </div>
       </div>
@@ -118,7 +105,7 @@
                       <div class="input-group-prepend">
                         <span class="input-group-text" id="basic-addon1">Category Name</span>
                       </div>
-                      <input type="text" class="form-control" placeholder="Category Name" required="">
+                      <input type="text" class="form-control" placeholder="Category Name" name = "C_Name" required="">
                     </div>
                   </div>
                 </div>
@@ -135,21 +122,7 @@
           </div>
         </div>
       </div>
-
-
-
-    <!-- Footer -->
-      <!-- <footer class="footer pt-0">
-        <div class="row align-items-center justify-content-lg-between">
-          <div class="col-lg-6">
-            <div class="copyright text-center  text-lg-left  text-muted">
-              &copy; 2021 <a class="font-weight-bold ml-1" target="_blank">Creative Team</a>
-            </div>
-          </div>
-        </div>
-      </footer> -->
       <?php include("footer.php")?>
-
     </div>
   </div>
   <!-- Argon Scripts -->
